05b25eec54dfb7787201a859d4c07b8cd318759d
CJI2019 <3000mn@naver.com>
2023-06-03 03:21:14 +0900

    찜 프레임 합체작업 및 홈 프레임 삭제하지 않도록조정
    추가로 텔레그램 데이터 리스트 삽입

--- Project/main.py
+++ Project/main.py
@@ -30,6 +30,7 @@ language = 'ko-KR'
 WINDOW_ROW = 1100
 WINDOW_COL = 700
 
+
 class MainGUI:
     def fetchMovieData(self):
         # Trend API 가져오기
@@ -40,6 +41,7 @@ class MainGUI:
         }
         tempR = requests.get(url, headers=headers)
         self.MovieD = json.loads(tempR.text)
+        print(self.MovieD)
         #print(self.MovieD["results"])
         self.printTrendMovie()
     def fetchTvData(self):
@@ -51,12 +53,14 @@ class MainGUI:
         }
         tempR = requests.get(url, headers=headers)
         self.TvD = json.loads(tempR.text)
+        print(self.TvD)
         self.printTrendTv()
     def printTrendMovie(self):
         self.imageM=[0]*21
         for i in range(len(self.MovieD["results"])):
             self.MimageUrl = "https://image.tmdb.org/t/p/w200" + str(self.MovieD["results"][i]["poster_path"])
             self.m = requests.get(self.MimageUrl)
+            noti.telegram_Movie_Trand += [self.MovieD["results"][i]]
 
             self.MImage = Image.open(BytesIO(self.m.content))
             self.MImage = self.MImage.convert('RGB')
@@ -67,7 +71,7 @@ class MainGUI:
             # 버튼 안에 이미지 넣기
             kindT="M"
             imageHeight = 290
-            self.MImageButton = Button(self.MFrame, image=self.imageM[i], anchor=N,height=imageHeight, command=lambda i=i: (goToDetail.GOTODETAIL(self.info_poster_label,self.window,self.imageM[i],self.MovieD["results"][i],kindT),self.ZzimImageChangeJudge()))
+            self.MImageButton = Button(self.MFrame, image=self.imageM[i], anchor=N,height=imageHeight, command=lambda i=i: (goToDetail.GOTODETAIL(self.info_poster_label,self.homeFrame,self.imageM[i],self.MovieD["results"][i],kindT),self.ZzimImageChangeJudge()))
             self.MImageButton.image = self.imageM[i]
             self.MImageButton.grid(row=0, column=i, sticky="ew")
 
@@ -81,6 +85,7 @@ class MainGUI:
         for i in range(len(self.TvD["results"])):
             self.imageUrl = "https://image.tmdb.org/t/p/w200" + str(self.TvD["results"][i]["poster_path"])
             self.r = requests.get(self.imageUrl)
+            noti.telegram_TV_Trand += [self.TvD['results'][i]]
 
             self.pilImage = Image.open(BytesIO(self.r.content))
             self.pilImage = self.pilImage.convert('RGB')
@@ -91,7 +96,7 @@ class MainGUI:
             # 버튼 안에 이미지 넣기
             kindT="T"
             imageHeight = 290
-            self.ImageButton = Button(self.TFrame, image=self.image[i], anchor=N,height=imageHeight, command=lambda i=i: (goToDetail.GOTODETAIL(self.info_poster_label,self.window,self.image[i],self.TvD["results"][i],kindT),self.ZzimImageChangeJudge()))
+            self.ImageButton = Button(self.TFrame, image=self.image[i], anchor=N,height=imageHeight, command=lambda i=i: (goToDetail.GOTODETAIL(self.info_poster_label,self.homeFrame,self.image[i],self.TvD["results"][i],kindT),self.ZzimImageChangeJudge()))
             self.ImageButton.image = self.image[i]
             self.ImageButton.grid(row=0, column=i, sticky="ew")
 
@@ -105,6 +110,7 @@ class MainGUI:
         url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={query}&language={language}'
         response = requests.get(url)
         data = response.json()
+
 
         results = data['results']
         self.search_movie_listbox.delete(0, END)      # 리스트 박스 요소 모두 제거
@@ -138,7 +144,7 @@ class MainGUI:
         index = self.search_movie_listbox.curselection()[0]
         if self.search_movie_list[index]:
             poster_path = self.search_movie_list[index]['poster_path']
-            poster_url = f'https://image.tmdb.org/t/p/w500/{poster_path}'
+            poster_url = f'https://image.tmdb.org/t/p/w500/{poster_path}' # /p2l6hOlyx1ZP6jt9wagthliVg9h.jpg
             response = requests.get(poster_url)
 
             img = Image.open(io.BytesIO(response.content))
@@ -147,9 +153,9 @@ class MainGUI:
 
             img = ImageTk.PhotoImage(img)
             if 'title' in self.search_movie_list[index]:
-                goToDetail.GOTODETAIL(self.info_poster_label, self.window, img, self.search_movie_list[index], "M")
+                goToDetail.GOTODETAIL(self.info_poster_label, self.homeFrame, img, self.search_movie_list[index], "M")
             else:
-                goToDetail.GOTODETAIL(self.info_poster_label, self.window, img, self.search_movie_list[index], "T")
+                goToDetail.GOTODETAIL(self.info_poster_label, self.homeFrame, img, self.search_movie_list[index], "T")
 
             self.ZzimImageChangeJudge()
         else:
@@ -167,6 +173,8 @@ class MainGUI:
                 self.ZzimButton.configure(image=self.Zzim_before)
                 self.ZzimButton.image = self.Zzim_before
                 self.zzim.del_zzim(goToDetail.currentPick)
+        self.Zzim_listbox_Sync()
+        self.voteRatio_Sync()
     def ZzimImageChangeJudge(self):
         if goToDetail.currentPick not in self.zzim.get_zzimlist():
             self.ZzimButton.configure(image=self.Zzim_before)
@@ -174,21 +182,18 @@ class MainGUI:
         else:
             self.ZzimButton.configure(image=self.Zzim_after)
             self.ZzimButton.image = self.Zzim_after
-    def OpenZzimFrame(self):
-        self.voteRatio_Chart()
-        new_frame = Toplevel(self.window, width=300, height=600)
-        new_frame.geometry('300x600+1200+100')
-        new_frame.title('찜 목록')
+    def CreateZzim_listboxFrame(self):
+        self.Zzim_listbox = Listbox(self.ZzimlistFrame, width=40, height=30)
+        self.Zzim_listbox.place(x=600, y=100)  # -3 은 리스트박스 경계선 안보이도록
+        self.Zzim_listbox.configure(bg='#FCD572')
+        Label(self.ZzimlistFrame, text='찜 목록').place(x=550+175,y=75)
 
+    def Zzim_listbox_Sync(self):
+        self.Zzim_list = []
+        self.Zzim_listbox.delete(0,END)
+        self.Zzim_listbox.bind('<<ListboxSelect>>', self.Zzim_select_show_movie_poster)
         # 딕셔너리 데이터
         movie_list = self.zzim.get_zzimlist()
-
-        self.Zzim_listbox = Listbox(new_frame, width=300, height=600, bg='light gray')
-        self.Zzim_listbox.place(x=-3, y=0)  # -3 은 리스트박스 경계선 안보이도록
-        self.Zzim_listbox.configure(bg='#FCD572')
-
-        self.Zzim_listbox.bind('<<ListboxSelect>>', self.Zzim_select_show_movie_poster)
-        self.Zzim_list = []
         # 딕셔너리의 키와 값을 리스트 박스에 추가
         for idx,result in enumerate(movie_list):
             if 'title' in result:
@@ -207,33 +212,32 @@ class MainGUI:
 
             img = Image.open(io.BytesIO(response.content))
             img = img.convert('RGB')
-            img = img.resize((190, 290), Image.NEAREST)
+            img = img.resize((300, 250), Image.LANCZOS)
             # NEAREST(빠름) , LANCZOS(느림)
 
             img = ImageTk.PhotoImage(img)
-            if 'title' in self.Zzim_list[index]:
-                goToDetail.GOTODETAIL(self.info_poster_label,self.window,img,self.Zzim_list[index],"M")
-            else:
-                goToDetail.GOTODETAIL(self.info_poster_label,self.window,img,self.Zzim_list[index],"T")
-
-            self.ZzimImageChangeJudge()
+            # 포스터
+            self.Zzim_poster_label = Label(self.ZzimlistFrame, width=310, height=260, bg='#EC7729')
+            self.Zzim_poster_label.configure(image=img)
+            self.Zzim_poster_label.image = img
+            self.Zzim_poster_label.place(x=140, y=45)
+            # self.ZzimImageChangeJudge()
         else:
             self.info_poster_label.configure(image='')
             self.info_poster_label.image = None
-    def voteRatio_Chart(self):
-        new_frame = Toplevel(self.window, width=320, height=350)
-        new_frame.geometry('320x400+1500+100')
-        new_frame.title('평점')
-
-        Label(new_frame, text='Movie Ratio').pack()
+    def CreateZZimList(self):
+        Label(self.ZzimlistFrame, text='평점').place(x=90+195,y=355-25)
+        self.CreateZzim_listboxFrame()
+        self.voteRatio_Sync()
+    def voteRatio_Sync(self):
+        # 찜 목록에 있는 평점 데이터를 담는 리스트
         movie_list = self.zzim.get_zzimlist()
 
-        # 찜 목록에 있는 평점 데이터를 담는 리스트
         data = [round(i['vote_average'],1) for i in movie_list]
         c_width = 400
         c_height = 350
-        c = Canvas(new_frame, width=c_width, height=c_height, bg='#FCD572')
-        c.pack()
+        c = Canvas(self.ZzimlistFrame, width=c_width, height=c_height, bg='#FCD572')
+        c.place(x=90,y=355)
         y_stretch = 30
         y_gap = 20
         x_stretch = 10
@@ -254,9 +258,9 @@ class MainGUI:
         self.homeFrame = Frame(self.window,bg="orange",width=1000,height=800)
         self.homeFrame.place(x=120,y=5)
 
-        if self.currentPageNum==1: #지도에서 홈으로 왔으면
-            self.mapFrame.destroy()
-        self.currentPageNum=0
+        # if self.currentPageNum==1: #지도에서 홈으로 왔으면
+        #     self.mapFrame.destroy()
+        # self.currentPageNum=0
 
         # 검색창
         self.search_entry = Entry(self.homeFrame, width=40)
@@ -320,8 +324,8 @@ class MainGUI:
         self.fontstyle2 = font.Font(self.homeFrame, size=16, weight='bold', family='Consolas')
 
         # api불러오기
-        #self.fetchMovieData()
-        #self.fetchTvData()
+        # self.fetchMovieData()
+        # self.fetchTvData()
 
         # 찜 버튼 만들기 (동그라미 버튼)
         image = Image.open("resource/Zzim_before.png")
@@ -340,20 +344,22 @@ class MainGUI:
         self.Zzim_after = ImageTk.PhotoImage(resized_image)
 
     def callMap(self):
-        if self.currentPageNum==0: #홈에서 지도로 왔으면
-            self.homeFrame.destroy()
-        self.currentPageNum=1
-
-
+        # if self.currentPageNum==0: #홈에서 지도로 왔으면
+        #     self.homeFrame.destroy()
+        # self.currentPageNum=1
         self.mapFrame = Frame(self.window,bg="orange",width=1000,height=800)
         self.mapFrame.place(x=120,y=5)
 
-        map.MAP(self.window, self.mapFrame,self.cefInitialNum)
+        self.map = map.MAP(self.window, self.mapFrame,self.cefInitialNum)
         #cef는 한번만 초기화 해야한다.
         self.cefInitialNum += 1
+     # -------------------------추가------------------------------
+    def show_frame(self, frame):
+        frame.tkraise()
     def tele_start(self):
         teller.telegram_start()
 
+    # -------------------------추가------------------------------
     def __init__(self):
         #현재 있는곳
         #0 : 홈, 1: 지도
@@ -364,17 +370,29 @@ class MainGUI:
         self.window.geometry("1100x700+100+100")
         self.window.configure(bg='#FFA500')
 
+        # 찜 변수 선언
+        self.zzim = zzim.ZZIM()
+
         #지도
         self.cefInitialNum=0
 
         self.home()
         #====================왼쪽 메뉴 바========================
         # 찜 목록 버튼
+
+        self.ZzimlistFrame = Frame(self.window, bg="orange", width=1000, height=800)
+        self.ZzimlistFrame.place(x=120, y=-20)
+
+        Label(self.ZzimlistFrame, bg="orange", width=1000, height=800).pack()
+        self.CreateZZimList()
+
+
         image = Image.open("resource/Zzimlist.png")
         image_with_alpha = image.convert("RGBA")
         resized_image = image_with_alpha.resize((100, 100))
         photo2 = ImageTk.PhotoImage(resized_image)
-        Button(self.window, image=photo2, command=self.OpenZzimFrame,highlightthickness=0).place(x=8, y=5)
+        # Button(self.window, image=photo2, command=self.OpenZzimFrame,highlightthickness=0).place(x=8, y=5)
+        Button(self.window,image=photo2,highlightthickness=0,command=lambda: self.show_frame(self.ZzimlistFrame)).place(x=8, y=5)
 
         image = Image.open("resource/telegram.png")
         image_with_alpha = image.convert("RGBA")
@@ -383,29 +401,29 @@ class MainGUI:
         # 텔레그램 버튼
         Button(self.window, image=photo3 ,highlightthickness=0,command=self.tele_start).place(x=8, y=125)
 
+        #홈버튼
+        image = Image.open("resource/home.png")
+        image_with_alpha = image.convert("RGBA")
+        resized_image = image_with_alpha.resize((100, 100))
+        photo5 = ImageTk.PhotoImage(resized_image)
+        # self.homeBtn = Button(self.window, image=photo5, highlightthickness=0,command =lambda:self.home())
+        Button(self.window,image=photo5,highlightthickness=0,command=lambda: self.show_frame(self.homeFrame)).place(x=8, y=360)
+        # self.homeBtn.place(x=8, y=360)
 
         # 지도 버튼
         image = Image.open("resource/Map.png")
         image_with_alpha = image.convert("RGBA")
         resized_image = image_with_alpha.resize((100, 100))
         photo4 = ImageTk.PhotoImage(resized_image)
+        # self.callMap()
         self.mapBtn = Button(self.window, image=photo4 ,highlightthickness=0,command=lambda: self.callMap())
+        # Button(self.window, image=photo4, command=lambda: self.show_frame(self.mapFrame)).place(x=8, y=245)
         self.mapBtn.place(x=8, y=245)
-
-        #홈버튼
-        image = Image.open("resource/Map.png")
-        image_with_alpha = image.convert("RGBA")
-        resized_image = image_with_alpha.resize((100, 100))
-        photo5 = ImageTk.PhotoImage(resized_image)
-        self.homeBtn = Button(self.window, image=photo5, highlightthickness=0,command =lambda:self.home())
-        self.homeBtn.place(x=8, y=360)
         #====================왼쪽 메뉴 바========================
 
-        # 찜 변수 선언
-        self.zzim = zzim.ZZIM()
-        self.window.mainloop()
-
+        self.show_frame(self.homeFrame)
 
         self.tel_count = 0
+        self.window.mainloop()
 
 MainGUI()
