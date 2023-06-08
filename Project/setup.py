9f0ba3febede871fa2f34fd8814013dc028dc612
junn216 <87471961+Junbeomb@users.noreply.github.com>
2023-06-08 13:24:32 +0900

    Create setup.py

---
+++ Project/setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+
+setup(name="MOVING",version='1.0',py_modules=['main','goToDetail','map','noti','setup','teller','zzim'],
+      data_files=[('resource',['resource/home.png','resource/Map.png','resource/telegram.png','resource/Zzim_after.png','resource/Zzim_before.png','resource/Zzimlist.png'])])
